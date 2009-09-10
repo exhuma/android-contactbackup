5cf7c77a28d13a97a06f4fbe762eafd618351d1f
Michel Albert <michel@albert.lu>
2009-09-10 10:08:23 +0200

    Prevented NPEs

--- src/lu/albert/android/contactbackup/BackupThread.java
+++ src/lu/albert/android/contactbackup/BackupThread.java
@@ -227,18 +227,22 @@ public class BackupThread extends Thread {
 		Cursor cursor = mParent.managedQuery(Photos.CONTENT_URI, null, where, null,
 				null);
 
-		int dataColumn = cursor.getColumnIndex(Photos.DATA);
-
 		JSONArray photos = new JSONArray();
 
-		if (cursor.moveToFirst()) {
-			do {
-				if (cursor.getBlob(dataColumn) != null) {
-					photos.put(Base64.encodeBytes(cursor
-							.getBlob(dataColumn)));
-				}
-			} while (cursor.moveToNext());
+		if ( cursor != null ) {
+			
+			int dataColumn = cursor.getColumnIndex(Photos.DATA);
+	
+			if (cursor.moveToFirst()) {
+				do {
+					if (cursor.getBlob(dataColumn) != null) {
+						photos.put(Base64.encodeBytes(cursor
+								.getBlob(dataColumn)));
+					}
+				} while (cursor.moveToNext());
+			}
 		}
+		
 		contact.put( ContactColumns.PHOTOS, photos);
 	}
 	
@@ -259,33 +263,38 @@ public class BackupThread extends Thread {
 		Cursor cursor = mParent.managedQuery(ContactMethods.CONTENT_URI, null, where, null,
 				null);
 		
-		int auxDataColumn = cursor.getColumnIndex(ContactMethods.AUX_DATA);
-		int dataColumn = cursor.getColumnIndex(ContactMethods.DATA);
-		int isPrimaryColumn = cursor.getColumnIndex(ContactMethods.ISPRIMARY);
-		int kindColumn = cursor.getColumnIndex(ContactMethods.KIND);
-		int labelColumn = cursor.getColumnIndex(ContactMethods.LABEL);
-		int typeColumn = cursor.getColumnIndex(ContactMethods.TYPE);
-		
 		JSONArray contactMethods = new JSONArray();
 
-		if (cursor.moveToFirst()) {
-			do {
-				JSONObject method = new JSONObject();
-				method.put(ContactColumns.ContactMethodColumns.IS_PRIMARY,
-						(cursor.getInt(isPrimaryColumn) != 0));
-				method.put(ContactColumns.ContactMethodColumns.LABEL,
-						cursor.getString(labelColumn));
-				method.put(ContactColumns.ContactMethodColumns.TYPE,
-						cursor.getString(typeColumn));
-				method.put(ContactColumns.ContactMethodColumns.AUX_DATA,
-						cursor.getString(auxDataColumn));
-				method.put(ContactColumns.ContactMethodColumns.DATA,
-						cursor.getString(dataColumn));
-				method.put(ContactColumns.ContactMethodColumns.KIND,
-						cursor.getString(kindColumn));
-				contactMethods.put(method);
-			} while (cursor.moveToNext());
+		if ( cursor != null ) {
+		
+			int auxDataColumn = cursor.getColumnIndex(ContactMethods.AUX_DATA);
+			int dataColumn = cursor.getColumnIndex(ContactMethods.DATA);
+			int isPrimaryColumn = cursor.getColumnIndex(ContactMethods.ISPRIMARY);
+			int kindColumn = cursor.getColumnIndex(ContactMethods.KIND);
+			int labelColumn = cursor.getColumnIndex(ContactMethods.LABEL);
+			int typeColumn = cursor.getColumnIndex(ContactMethods.TYPE);
+			
+	
+			if (cursor.moveToFirst()) {
+				do {
+					JSONObject method = new JSONObject();
+					method.put(ContactColumns.ContactMethodColumns.IS_PRIMARY,
+							(cursor.getInt(isPrimaryColumn) != 0));
+					method.put(ContactColumns.ContactMethodColumns.LABEL,
+							cursor.getString(labelColumn));
+					method.put(ContactColumns.ContactMethodColumns.TYPE,
+							cursor.getString(typeColumn));
+					method.put(ContactColumns.ContactMethodColumns.AUX_DATA,
+							cursor.getString(auxDataColumn));
+					method.put(ContactColumns.ContactMethodColumns.DATA,
+							cursor.getString(dataColumn));
+					method.put(ContactColumns.ContactMethodColumns.KIND,
+							cursor.getString(kindColumn));
+					contactMethods.put(method);
+				} while (cursor.moveToNext());
+			}
 		}
+		
 		contact.put(ContactColumns.CONTACT_METHODS, contactMethods);
 		
 	}
@@ -302,43 +311,40 @@ public class BackupThread extends Thread {
 	 *             Thrown when data could not be successfully encoded to
 	 *             JSON
 	 */
-	private void appendPhoneNumbers(JSONObject contact) throws JSONException
-			{
+	private void appendPhoneNumbers(JSONObject contact)
+		throws JSONException {
 
-		String where;
-		try{
-			where = Phones.PERSON_ID + "=" + contact.getInt( ContactColumns.ID );
-		} catch (JSONException e) {
-			System.err.println("unable to get contact: " + e.getMessage()); // TODO logging
-			return;
-		}
+		String where = Phones.PERSON_ID + "=" + contact.getInt( ContactColumns.ID );
 		Cursor cursor = mParent.managedQuery(Phones.CONTENT_URI, null, where, null,
 				null);
-
-		int isPrimaryColumn = cursor.getColumnIndex(Phones.ISPRIMARY);
-		int labelColumn = cursor.getColumnIndex(Phones.LABEL);
-		int numberColumn = cursor.getColumnIndex(Phones.NUMBER);
-		int numberKeyColumn = cursor.getColumnIndex(Phones.NUMBER_KEY);
-		int typeColumn = cursor.getColumnIndex(Phones.TYPE);
-
 		JSONArray phonenumbers = new JSONArray();
 
-		if (cursor.moveToFirst()) {
-			do {
-				JSONObject number = new JSONObject();
-				number.put(ContactColumns.PhoneColumns.IS_PRIMARY,
-						(cursor.getInt(isPrimaryColumn) != 0));
-				number.put(ContactColumns.PhoneColumns.LABEL,
-						cursor.getString(labelColumn));
-				number.put(ContactColumns.PhoneColumns.NUMBER,
-						cursor.getString(numberColumn));
-				number.put(ContactColumns.PhoneColumns.NUMBER_KEY,
-						cursor.getString(numberKeyColumn));
-				number.put(ContactColumns.PhoneColumns.TYPE,
-						cursor.getString(typeColumn));
-				phonenumbers.put(number);
-			} while (cursor.moveToNext());
+		if ( cursor != null ) {
+
+			int isPrimaryColumn = cursor.getColumnIndex(Phones.ISPRIMARY);
+			int labelColumn = cursor.getColumnIndex(Phones.LABEL);
+			int numberColumn = cursor.getColumnIndex(Phones.NUMBER);
+			int numberKeyColumn = cursor.getColumnIndex(Phones.NUMBER_KEY);
+			int typeColumn = cursor.getColumnIndex(Phones.TYPE);
+	
+			if (cursor.moveToFirst()) {
+				do {
+					JSONObject number = new JSONObject();
+					number.put(ContactColumns.PhoneColumns.IS_PRIMARY,
+							(cursor.getInt(isPrimaryColumn) != 0));
+					number.put(ContactColumns.PhoneColumns.LABEL,
+							cursor.getString(labelColumn));
+					number.put(ContactColumns.PhoneColumns.NUMBER,
+							cursor.getString(numberColumn));
+					number.put(ContactColumns.PhoneColumns.NUMBER_KEY,
+							cursor.getString(numberKeyColumn));
+					number.put(ContactColumns.PhoneColumns.TYPE,
+							cursor.getString(typeColumn));
+					phonenumbers.put(number);
+				} while (cursor.moveToNext());
+			}
 		}
+		
 		contact.put(ContactColumns.PHONE_NUMBERS, phonenumbers);
 
 	}
@@ -355,36 +361,40 @@ public class BackupThread extends Thread {
 	 *             Thrown when data could not be successfully encoded to
 	 *             JSON
 	 */
-	private void appendOrganizations(JSONObject contact) throws JSONException
-			{
+	private void appendOrganizations(JSONObject contact)
+		throws JSONException {
 
 		String where = Organizations.PERSON_ID + "=" + contact.getInt( ContactColumns.ID );
 		Cursor cursor = mParent.managedQuery(Organizations.CONTENT_URI, null, where, null,
 				null);
 
-		int isPrimaryColumn = cursor.getColumnIndex(Organizations.ISPRIMARY);
-		int labelColumn = cursor.getColumnIndex(Organizations.LABEL);
-		int typeColumn = cursor.getColumnIndex(Organizations.TYPE);
-		int companyColumn = cursor.getColumnIndex(Organizations.COMPANY);
-		int titleColumn = cursor.getColumnIndex(Organizations.TITLE);
 		JSONArray organizations = new JSONArray();
-
-		if (cursor.moveToFirst()) {
-			do {
-				JSONObject org = new JSONObject();
-				org.put(OrganizationColumns.IS_PRIMARY,
-						(cursor.getInt(isPrimaryColumn) != 0));
-				org.put(OrganizationColumns.LABEL,
-						cursor.getString(labelColumn));
-				org.put(OrganizationColumns.TITLE,
-						cursor.getString(titleColumn));
-				org.put(OrganizationColumns.COMPANY,
-						cursor.getString(companyColumn));
-				org.put(OrganizationColumns.TYPE,
-						cursor.getString(typeColumn));
-				organizations.put(org);
-			} while (cursor.moveToNext());
+		
+		if ( cursor != null ) {
+			int isPrimaryColumn = cursor.getColumnIndex(Organizations.ISPRIMARY);
+			int labelColumn = cursor.getColumnIndex(Organizations.LABEL);
+			int typeColumn = cursor.getColumnIndex(Organizations.TYPE);
+			int companyColumn = cursor.getColumnIndex(Organizations.COMPANY);
+			int titleColumn = cursor.getColumnIndex(Organizations.TITLE);
+	
+			if (cursor.moveToFirst()) {
+				do {
+					JSONObject org = new JSONObject();
+					org.put(OrganizationColumns.IS_PRIMARY,
+							(cursor.getInt(isPrimaryColumn) != 0));
+					org.put(OrganizationColumns.LABEL,
+							cursor.getString(labelColumn));
+					org.put(OrganizationColumns.TITLE,
+							cursor.getString(titleColumn));
+					org.put(OrganizationColumns.COMPANY,
+							cursor.getString(companyColumn));
+					org.put(OrganizationColumns.TYPE,
+							cursor.getString(typeColumn));
+					organizations.put(org);
+				} while (cursor.moveToNext());
+			}
 		}
+		
 		contact.put(ContactColumns.ORGANIZATIONS, organizations);
 
 	}
